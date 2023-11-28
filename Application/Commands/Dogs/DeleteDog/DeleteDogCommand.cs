5170bf5e772b5b88999a5f35f46ebc0ed2a406f3
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2023-11-28 12:21:52 +0100

    Cleanup

--- Application/Commands/Dogs/DeleteDog/DeleteDogCommand.cs
+++ Application/Commands/Dogs/DeleteDog/DeleteDogCommand.cs
@@ -1,11 +1,4 @@
-﻿using Application.Dtos;
-using Domain.Models;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace Application.Commands.Dogs.DeleteDog
 {
