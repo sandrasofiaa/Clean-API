1ad25617f314de6bb8f0dfdca47c633903188036
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2023-11-28 13:04:43 +0100

    Cat tests

---
+++ Test/CatTests/CommandTest/AddCatTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using Application.Dtos;
+using Domain.Models;
+using Infrastructure.Database;
+using Application.Commands.Cats.AddCat;
+
+namespace Test.CatTests.CommandTest
+{
+    [TestFixture]
+    public class AddCatTest
+    {
+        private AddCatCommandHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize the handler and mock database before each test
+            _mockDatabase = new MockDatabase();
+            _handler = new AddCatCommandHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task GivenValidCatDto_AddCat_ReturnsNewCat()
+        {
+            // Arrange
+            var CatDto = new CatDto { Name = "Nisse", LikesToPlay = true };
+            var addCatCommand = new AddCatCommand(CatDto);
+
+            // Act
+            Cat addedCat = await _handler.Handle(addCatCommand, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(addedCat);
+            Assert.That(addedCat.Name, Is.EqualTo(CatDto.Name));
+            Assert.That(addedCat.LikesToPlay, Is.EqualTo(CatDto.LikesToPlay));
+            Assert.IsTrue(_mockDatabase.Cats.Contains(addedCat));
+        }
+
+        [Test]
+        public void GivenInvalidCatDto_AddCat_ThrowsArgumentException()
+        {
+            // Arrange
+            var invalidCatDto = new CatDto { Name = "", LikesToPlay = false };
+            var addCatCommand = new AddCatCommand(invalidCatDto);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () => await _handler.Handle(addCatCommand, CancellationToken.None));
+        }
+    }
+}
---
+++ Test/CatTests/QueryTest/GetAllCatsTest.cs
@@ -0,0 +1,46 @@
+﻿using Application.Queries.Cats;
+using Application.Queries.Cats.GetAll;
+using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test.CatTests.QueryTest
+{
+    [TestFixture]
+    public class GetAllCatsTest
+    {
+        private GetAllCatsQueryHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new GetAllCatsQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ReturnsAllCatsFromMockDatabase()
+        {
+            // Arrange
+            var catsList = _mockDatabase.Cats;
+
+            var query = new GetAllCatsQuery();
+
+            // Act
+            List<Cat> result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.Count, Is.EqualTo(catsList.Count));
+            Assert.IsTrue(result.SequenceEqual(catsList)); // Check if both lists have the same elements
+        }
+    }
+}
+
