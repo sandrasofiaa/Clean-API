84d9404d08c4af17f19e587de25753d811c9a7cd
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2023-12-19 11:24:50 +0100

    -

--- Application/Commands/Dogs/UpdateDog/UpdateDogByIdCommandHandler.cs
+++ Application/Commands/Dogs/UpdateDog/UpdateDogByIdCommandHandler.cs
@@ -47,72 +47,9 @@ namespace Application.Commands.Dogs.UpdateDog
                 catch (Exception ex)
                 {
                     // Handle exceptions
-                    throw ex;
+                    throw;
                 }
             }
         }
-        //public class UpdateDogByIdCommandHandler : IRequestHandler<UpdateDogByIdCommand, Dog>
-        //{
-        //    private readonly IAnimalRepository _animalRepository;
-        //    private readonly DogValidator _dogValidator;
-        //    private readonly GuidValidator _guidValidator;
-
-        //    public UpdateDogByIdCommandHandler(IAnimalRepository animalRepository, DogValidator dogValidator, GuidValidator guidValidator)
-        //    {
-        //        _animalRepository = animalRepository;
-        //        _dogValidator = dogValidator;
-        //        _guidValidator = guidValidator;
-        //    }
-
-        //    public async Task<Dog> Handle(UpdateDogByIdCommand request, CancellationToken cancellationToken)
-        //    {
-        //        var dogDto = request.UpdatedDog; // Assuming UpdatedDog is of type DogDto
-
-        //        var dogValidationResult = _dogValidator.Validate(dogDto);
-        //        var guidValidationResult = _guidValidator.Validate(request.Id);
-
-        //        if (!dogValidationResult.IsValid || !guidValidationResult.IsValid)
-        //        {
-        //            // Handle validation errors
-        //            var errorMessages = dogValidationResult.Errors
-        //                .Concat(guidValidationResult.Errors)
-        //                .Select(error => error.ErrorMessage);
-
-        //            // For example, throwing a custom exception with validation error messages
-        //            throw new ValidationException(string.Join(", ", errorMessages));
-        //        }
-
-        //        try
-        //        {
-        //            // Fetch the dog to update from the repository using a more generic method
-        //            Dog dogToUpdate = (Dog)await _animalRepository.GetByIdAsync(request.Id);
-
-        //            if (dogToUpdate != null)
-        //            {
-        //                // Update the properties of the dog
-        //                dogToUpdate.Name = dogDto.Name;
-        //                dogToUpdate.Breed = dogDto.Breed;
-        //                dogToUpdate.Weight = (int)dogDto.Weight;
-
-        //                // Call your repository method to update the dog in the database
-        //                await _animalRepository.UpdateAnimalAsync(dogToUpdate);
-
-        //                // Return the updated dog
-        //                return dogToUpdate;
-        //            }
-
-        //            // Handle if the dog is not found
-        //            return null; // or throw an exception
-        //        }
-        //        catch (ValidationException)
-        //        {
-        //            throw;
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            // Handle other exceptions
-        //            throw ex;
-        //        }
-        //    }
     }
 }
