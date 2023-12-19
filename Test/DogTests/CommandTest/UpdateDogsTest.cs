637d73dc768f67644d28c8a489cc045361c24046
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2023-12-19 11:20:04 +0100

    fix

--- Test/DogTests/CommandTest/UpdateDogsTest.cs
+++ Test/DogTests/CommandTest/UpdateDogsTest.cs
@@ -9,38 +9,35 @@ using Test.TestHelpers;
 
 namespace Test.DogTests.CommandTest
 {
+
     [TestFixture]
     public class UpdateDogsTest
     {
-        [TestFixture]
-        public class UpdateDogByIdCommandHandlerTests
+        private UpdateDogByIdCommandHandler _handler;
+        private Mock<IAnimalRepository> _animalRepositoryMock;
+
+        [SetUp]
+        public void Setup()
         {
-            private UpdateDogByIdCommandHandler _handler;
-            private Mock<IAnimalRepository> _animalRepositoryMock;
+            _animalRepositoryMock = new Mock<IAnimalRepository>();
+            _handler = new UpdateDogByIdCommandHandler(_animalRepositoryMock.Object);
+        }
 
-            [SetUp]
-            public void Setup()
-            {
-                _animalRepositoryMock = new Mock<IAnimalRepository>();
-                _handler = new UpdateDogByIdCommandHandler(_animalRepositoryMock.Object);
-            }
+        [Test]
+        [CustomAutoData]
+        public async Task WHEN_Handle_THEN_UpdatesDog([Frozen] Dog initialDog, DogDto updatedDog)
+        {
+            // Arrange
+            _animalRepositoryMock.Setup(x => x.GetByIdAsync(initialDog.AnimalId)).ReturnsAsync(initialDog);
 
-            [Test]
-            [CustomAutoData]
-            public async Task WHEN_Handle_THEN_UpdatesDog([Frozen] Dog initialDog, DogDto updatedDog)
-            {
-                // Arrange
-                _animalRepositoryMock.Setup(x => x.GetByIdAsync(initialDog.AnimalId)).ReturnsAsync(initialDog);
+            var command = new UpdateDogByIdCommand(updatedDog, initialDog.AnimalId);
 
-                var command = new UpdateDogByIdCommand(updatedDog, initialDog.AnimalId);
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
 
-                // Act
-                var result = await _handler.Handle(command, CancellationToken.None);
-
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result, Is.InstanceOf<Dog>());
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<Dog>());
         }
     }
 }
