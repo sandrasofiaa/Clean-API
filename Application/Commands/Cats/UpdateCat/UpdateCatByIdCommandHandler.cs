c804073e7967f662d410d32b2cb9906c2d920f6e
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2023-12-19 13:02:25 +0100

    fixat mellanslag

--- Application/Commands/Cats/UpdateCat/UpdateCatByIdCommandHandler.cs
+++ Application/Commands/Cats/UpdateCat/UpdateCatByIdCommandHandler.cs
@@ -2,9 +2,9 @@
 using Infrastructure.Interface;
 using MediatR;
 
-namespace Application.Commands.Cats.UpdateCat 
+namespace Application.Commands.Cats.UpdateCat
 {
-    public class UpdateCatByIdCommandHandler : IRequestHandler<UpdateCatByIdCommand, Cat> // Updated class name and return type
+    public class UpdateCatByIdCommandHandler : IRequestHandler<UpdateCatByIdCommand, Cat> //Updated class name and return type
     {
         private readonly IAnimalRepository _animalRepository;
 
@@ -13,35 +13,35 @@ namespace Application.Commands.Cats.UpdateCat
             _animalRepository = animalRepository;
         }
 
-        public async Task<Cat> Handle(UpdateCatByIdCommand request, CancellationToken cancellationToken) // Updated method signature
+        public async Task<Cat> Handle(UpdateCatByIdCommand request, CancellationToken cancellationToken) //Updated method signature
         {
-            var catDto = request.UpdatedCat; // Assuming UpdatedCat is of type CatDto
+            var catDto = request.UpdatedCat; //Assuming UpdatedCat is of type CatDto
 
             try
             {
-                // Fetch the cat to update from the repository using a more generic method
-                Cat catToUpdate = (Cat)await _animalRepository.GetByIdAsync(request.Id); // Updated variable names
+                //Fetch the cat to update from the repository using a more generic method
+                Cat catToUpdate = (Cat)await _animalRepository.GetByIdAsync(request.Id); //Updated variable names
 
                 if (catToUpdate != null)
                 {
-                    // Update the properties of the cat
+                    //Update the properties of the cat
                     catToUpdate.Name = catDto.Name;
                     catToUpdate.Breed = catDto.Breed;
                     catToUpdate.Weight = (int)catDto.Weight;
 
-                    // Call your repository method to update the cat in the database
+                    //Call your repository method to update the cat in the database
                     await _animalRepository.UpdateAnimalAsync(catToUpdate);
 
-                    // Return the updated cat
+                    //Return the updated cat
                     return catToUpdate;
                 }
 
                 // Handle if the cat is not found
-                return null; // or throw an exception
+                return null; //or throw an exception
             }
             catch (Exception ex)
             {
-                // Handle exceptions
+                //Handle exceptions
                 throw;
             }
         }
